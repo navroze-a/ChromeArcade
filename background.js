75161228db58a9ed89be013e437e191b319c8a2f
Navrozee73 <navrozeantia@gmail.com>
2021-02-16 21:31:17 -0500

    Updated Menu
    Made the score hover and menu work. Currenlty, clicking any game besides pacman takes u to missile commander

--- background.js
+++ background.js
@@ -1,30 +1,44 @@
-chrome.runtime.onInstalled.addListener(function () {   // anon function to run when listener hears it installed
+chrome.runtime.onInstalled.addListener(function () {   // anon function to run when listener hears it installed 
 
-    // // sets color in storage to be #3aa757 (green), 
-    // //  then runs anon func to write in console      
-    // chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    //   console.log("The color saved to chrome's storage is now green");
-    // });
-  
-    chrome.storage.sync.set({ missleCommanderHighScore: 0}, function() {
-        console.log("set missle commander high score to 0");
-    }
+  // // sets color in storage to be #3aa757 (green), 
+  // //  then runs anon func to write in console      
+  // chrome.storage.sync.set({ color: '#3aa757' }, function () {
+  //   console.log("The color saved to chrome's storage is now green");
+  // });
 
-    );
-    // Adds declared rules to the script to activate when listener event occurs:
-  
-    //  Since added rules are saved across borwser restarts, we always register new rules as follows:
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-      // A rule performs an action when a set of conditions are met.
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({  //pageStateMatcher matches webpages iff all listed criteria are met. 
-            pageUrl: { hostEquals: 'developer.chrome.com' },  //Here, only criterion is that we're on developer.chrome.com
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()] // if the condition is met (we on developer.chrome.com), then we show a page action for the page
-      }]);
-    });
-  
+  chrome.storage.sync.set({ missleCommanderHighScore: 0 }, function () {
+    console.log("set missle commander high score to 0");
   });
-  
+
+  chrome.storage.sync.set({pacmanHighScore: 0}, function () {
+    console.log("set pacman high score to 104");
+  });
+
+  chrome.storage.sync.set({pongHighScore: 0}, function() {
+    console.log("set pong high score to 0");
+  });
+
+  chrome.storage.sync.set({breakoutHighScore: 0}, function() {
+    console.log("set breakout high score to 0");
+  });
+
+  chrome.storage.sync.set({snakeHighScore: 0}, function() {
+    console.log("set snake high score to 0");
+  });
+
+
+  // Adds declared rules to the script to activate when listener event occurs:
+
+  //  Since added rules are saved across borwser restarts, we always register new rules as follows:
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    // A rule performs an action when a set of conditions are met.
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({  //pageStateMatcher matches webpages iff all listed criteria are met. 
+          pageUrl: { hostEquals: 'developer.chrome.com' },  //Here, only criterion is that we're on developer.chrome.com
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()] // if the condition is met (we on developer.chrome.com), then we show a page action for the page
+    }]);
+  });
+});
--- index.js
+++ index.js
@@ -1,8 +1,76 @@
 let missleCommandButton = document.getElementById("missleCommanderButton");
+let scoreButton = document.getElementById("scoreButton");
 
-missleCommandButton.onclick = function(element) {
+let pacmanButton = document.getElementById("pacmanButton");
+let pongButton = document.getElementById("pongButton");
+let breakoutButton = document.getElementById("breakoutButton");
+let snakeButton = document.getElementById("snakeButton");
+
+/*
+missleCommandButton.onclick = function (element) {
     console.log("button clicked");
     window.location.href = "missileCommander.html";
 };
+*/
+
+pacmanButton.onclick = function(element) {
+    console.log("pacman game button clicked");
+    window.location.href = "pacmanPauseScreen.html";
+}
+
+pongButton.onclick = function(element) {
+    console.log("pong game button clicked");
+    window.location.href = "missileCommander.html";
+}
+
+breakoutButton.onclick = function(element) {
+    console.log("breakout game button clicked");
+    window.location.href = "missileCommander.html";
+}
+
+snakeButton.onclick = function(element) {
+    console.log("snake game button clicked");
+    window.location.href = "missileCommander.html";
+}
+
+
+
+
+scoreButton.onmouseover = function (element) {
+
+
+    console.log("showing scores");
+
+    chrome.storage.sync.get(['pacmanHighScore'], function (result) {
+        console.log('Pacman score currently is ' + result.pacmanHighScore + 'and is being displayed');
+        document.getElementById("pacmanButton").innerHTML = result.pacmanHighScore;
+    });
+
+    chrome.storage.sync.get(['pongHighScore'], function (result) {
+        console.log('Pong score currently is ' + result.pongHighScore + 'and is being displayed');
+        document.getElementById("pongButton").innerHTML = result.pongHighScore;
+    });
+
+    chrome.storage.sync.get(['breakoutHighScore'], function (result) {
+        console.log('Breakout score currently is ' + result.breakoutHighScore + 'and is being displayed');
+        document.getElementById("breakoutButton").innerHTML = result.breakoutHighScore;
+    });
+
+    chrome.storage.sync.get(['snakeHighScore'], function (result) {
+        console.log('snake score currently is ' + result.snakeHighScore + 'and is being displayed');
+        document.getElementById("snakeButton").innerHTML = result.snakeHighScore;
+    });
+};
+
+scoreButton.onmouseout = function (element) {
+    console.log("back to main screen");
+
+    pacmanButton.innerHTML = "PACMAN";
+    pongButton.innerHTML = "PONG";
+    breakoutButton.innerHTML = "BREAKOUT";
+    snakeButton.innerHTML = "SNAKE";
+};
+
+
 
 // icon comes from https://www.vecteezy.com/vector-art/120089-arcade-button-vector
